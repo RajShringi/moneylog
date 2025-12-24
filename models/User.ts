4c91bbf86c5dedeb0202c62a3e56a5a5c14b5694
Raj Shringi <raj24shringi@gmail.com>
2025-12-24 13:34:57 +0530

    create User model

---
+++ models/User.ts
@@ -0,0 +1,34 @@
+import { Document, model, models, Schema } from "mongoose";
+
+export interface IUser extends Document {
+  username: string;
+  email: string;
+  password: string;
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = models.User || model<IUser>("User", userSchema);
+
+export default User;
