f950df2fa1fce3c48bd6ae2bd3d9989ef5f5840a
Raj Shringi <raj24shringi@gmail.com>
2026-03-12 21:13:02 +0530

    store categories name in lowercase to prevent cased base duplication

--- schemas/categorySchema.ts
+++ schemas/categorySchema.ts
@@ -10,6 +10,7 @@ const categoryColorKeys = Object.keys(CATEGORY_COLORS) as [
 export const categorySchema = z.object({
   name: z
     .string()
+    .lowercase()
     .trim()
     .min(3, { message: "Category must be at least 3 characters long" }),
   type: z.enum(TRANSACTION_TYPES),
