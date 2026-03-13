df89ab1db2c2dc0ec06b3c3578df9d46b7fc40ba
Raj Shringi <raj24shringi@gmail.com>
2026-03-13 13:02:47 +0530

    Display categories on categories page

---
+++ components/CategorySection.tsx
@@ -0,0 +1,44 @@
+import { CATEGORY_COLORS } from "@/constants";
+import { CategoryColorKey, ICategory } from "@/types/category.types";
+import { Button } from "./ui/button";
+import { EllipsisVertical } from "lucide-react";
+
+interface CategorySectionProps {
+  title: string;
+  categories: ICategory[];
+}
+
+export default function CategorySection({
+  title,
+  categories,
+}: CategorySectionProps) {
+  return (
+    <div className="flex flex-col gap-2">
+      <h4 className="font-medium">{title}</h4>
+      {categories.length === 0 ? (
+        <div className="text-center font-bold text-lg">
+          There are no categories
+        </div>
+      ) : (
+        <div className="grid grid-cols-5 gap-2">
+          {categories.map((cat) => (
+            <div
+              key={cat._id}
+              className="flex items-center justify-between py-2 px-4 shadow bg-white rounded-lg"
+              style={{
+                borderLeft: `5px solid ${CATEGORY_COLORS[cat.color as CategoryColorKey].bg}`,
+              }}
+            >
+              <div className="flex items-center gap-1">
+                <div>{cat.name}</div>
+              </div>
+              <Button variant={"ghost"}>
+                <EllipsisVertical />
+              </Button>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
