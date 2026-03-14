99cbd7a73ddcb320c35f41ed32b3510f808a44a0
Raj Shringi <raj24shringi@gmail.com>
2026-03-14 21:38:24 +0530

    add TableActions component

---
+++ components/table/TableActions.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { MoreHorizontal } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { Button } from "../ui/button";
+
+interface TableActions {
+  id: string;
+}
+
+export default function TableActions({ id }: TableActions) {
+  return (
+    <div>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="h-8 w-8 p-0">
+            <span className="sr-only">Open menu</span>
+            <MoreHorizontal className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+          {/* <DropdownMenuItem
+            onClick={() => navigator.clipboard.writeText(payment.id)}
+          >
+            Copy payment ID
+          </DropdownMenuItem> */}
+          <DropdownMenuItem>Edit</DropdownMenuItem>
+          <DropdownMenuItem>Delete</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+}
