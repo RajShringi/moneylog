dafb671323c27f76ee2a631d46ca291b87cf5695
Raj Shringi <raj24shringi@gmail.com>
2026-03-21 00:25:36 +0530

    use AlertDialog for category archiving and wrap the edit link in a button.

--- components/CategoryCardActions.tsx
+++ components/CategoryCardActions.tsx
@@ -5,6 +5,17 @@ import Link from "next/link";
 import { archivedCategory } from "@/features/categories/actions";
 import { toast } from "sonner";
 import { useTransition } from "react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "./ui/alert-dialog";
 
 export default function CategoryCardActions({ id }: { id: string }) {
   const [isPending, startTransition] = useTransition();
@@ -22,18 +33,48 @@ export default function CategoryCardActions({ id }: { id: string }) {
   }
   return (
     <div className="flex gap-2">
-      <Link href={`/dashboard/categories/${id}/edit`}>
-        <Button className="cursor-pointer" variant={"ghost"}>
-          <Pencil />
-        </Button>
-      </Link>
       <Button
-        onClick={handleArchive}
+        asChild
+        disabled={isPending}
         className="cursor-pointer"
         variant={"ghost"}
       >
-        <Archive />
+        <Link href={`/dashboard/categories/${id}/edit`}>
+          <Pencil />
+        </Link>
       </Button>
+
+      <AlertDialog>
+        <AlertDialogTrigger asChild>
+          <Button
+            variant="ghost"
+            className="cursor-pointer"
+            disabled={isPending}
+          >
+            <span className="sr-only">delete transaction</span>
+            <Archive className="h-4 w-4" />
+          </Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Archive Category</AlertDialogTitle>
+            <AlertDialogDescription>
+              Are you sure you want to archive this category? This action cannot
+              be undone.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel disabled={isPending}>Cancel</AlertDialogCancel>
+            <AlertDialogAction
+              variant={"destructive"}
+              onClick={handleArchive}
+              disabled={isPending}
+            >
+              {isPending ? "Archiving..." : "Archive"}
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </div>
   );
 }
